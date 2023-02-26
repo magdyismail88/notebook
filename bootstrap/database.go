20bf5f83e420061995538e95be309a3f95e380ba
Magdy Ismail <magdyismail8888@gmail.com>
2023-02-26 19:42:32 +0200

    Added bootstrap database

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import "database/sql"
 
+// Open database connection
 func NewDBConnection(env *Env) *sql.DB {
 	conn, err := sql.Open(env.DatabaseAdapter, env.DatabaseName)
 	if err != nil {
@@ -10,6 +11,7 @@ func NewDBConnection(env *Env) *sql.DB {
 	return conn
 }
 
+// Close database connectino
 func CloseDBConnection(conn *sql.DB) {
 	if conn == nil {
 		return
